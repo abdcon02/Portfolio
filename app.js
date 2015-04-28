418070c11a36ae3b52eb816930689b744136a9f3
Connor Abdelnoor <connor.abd@gmail.com>
2015-04-28 11:39:24 -0700

    Add progress bar html

--- app.js
+++ app.js
@@ -2,11 +2,21 @@ var portfolio = angular.module('portfolio', ['ui.router']);
 
 portfolio.directive('enter', function() {
     return function(scope, element) {
-        element.bind('mouseenter', function() {
-            console.log(scope.apple);
-        });
+            element.bind('mouseenter', function() {
+                scope.result = element.text();
+            });
     };
 });
+
+portfolio.directive('update', function() {
+    return {
+        scope:{
+            fruit: "="
+        },
+        template: "<h3>This is your choice: {{fruit}} </h3>"
+    }
+})
+
 
   portfolio.config(function($stateProvider) {
 
--- controllers/ProjectController.js
+++ controllers/ProjectController.js
@@ -1,6 +1,8 @@
 portfolio.controller('ProjectCtrl', function($scope, $stateParams) {
-    $scope.apple = "Apple";
-    $scope.banana = "Banana";
-    $scope.melon = "Melon";
-    $scope.grape = "Grape";
+    $scope.apple = {type: "Apple", price: 4};
+    $scope.banana = {type: "Banana", price: 2};
+    $scope.melon = {type: "Melon", price: 44};
+    $scope.grape = {type: "Grape", price: 21};
+
+    $scope.result = "aa";
 });
